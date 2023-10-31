9d435635e7ac44a0af7720c29ad80bf3b49c95bc
Programmer Titu <149326743+ProgrammerTitu@users.noreply.github.com>
2023-11-01 02:50:11 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+
+$(document).ready(function(){
+
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.center').slick({
+    centerMode: true,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+      
+});
+
+// $('.one-time').slick({
+//   dots: true,
+//   infinite: true,
+//   autoplay: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
